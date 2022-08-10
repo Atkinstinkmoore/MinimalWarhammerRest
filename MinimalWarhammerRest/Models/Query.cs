9a75f981d31f6d74f553676ace1d467cf5810334
Atkinstinkmoore <44531198+Atkinstinkmoore@users.noreply.github.com>
2022-08-10 19:36:59 +0200

    add cached service to graphql
    add id query to graphql

--- MinimalWarhammerRest/Models/Query.cs
+++ MinimalWarhammerRest/Models/Query.cs
@@ -1,11 +1,28 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using MinimalWarhammerRest.Domain.DTOs;
+using MinimalWarhammerRest.Services;
 
 namespace MinimalWarhammerRest.Models
 {
     public class Query
     {
-        public IQueryable<Figure> GetFigures([Service] WarhammerDBContext context) =>
-            context.Figures.AsNoTracking();
+        public async Task<IQueryable<MiniatureDTO>> GetMinis([Service] IMiniatureService service)
+        {
+            var figures = await service.GetAll();
+            return figures.AsQueryable();
+        }
+        public async Task<MiniatureDTO?> GetMiniById([Service] IMiniatureService service, int id)
+        {
+            var figures = await service.Get(id);
+
+            return figures.Match(s =>
+            {
+                return s;
+            }, ex =>
+            {
+                return null;
+            });
+        }
     }
 
 }
